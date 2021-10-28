5fe86a58ad4213341af09088a3177edafa44e739
Vasilis Vlasopoulos <f3v3r.ath.cx@gmail.com>
2021-10-28 19:57:44 +0300

    added time log

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        SpaceSearcher searcher = new SpaceSearcher(15);
+        SpaceSearcher searcher = new SpaceSearcher(8);
 
         long start = System.currentTimeMillis();
-        State result = searcher.BeamSearch(500, 1000);
+        State result = searcher.BeamSearch(100, 1000);
         long end = System.currentTimeMillis();
 
         if ( result == null) System.out.println("Solution not found");
