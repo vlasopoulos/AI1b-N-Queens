4f525acc439113c4a8cfec43ebf14902f05c3ed0
Vasilis V <f3v3r.ath.cx@gmail.com>
2021-10-29 18:07:46 +0300

    Split Beam Search to BeamSearch and BeamSearchWithRR

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int n = 8;
         int k = 20;
         int maxIterations = 500;
+        boolean useRR = false;
 
         if (args.length == 1) {
             n = Integer.parseInt(args[0]);
@@ -17,10 +18,19 @@ public class Main {
             maxIterations = Integer.parseInt(args[2]);
         }
 
+        if (args.length == 4){
+            n = Integer.parseInt(args[0]);
+            k = Integer.parseInt(args[1]);
+            maxIterations = Integer.parseInt(args[2]);
+            if (args[3].equals("true")) useRR = true;
+        }
+
         SpaceSearcher searcher = new SpaceSearcher(n);
 
         long start = System.currentTimeMillis();
-        State result = searcher.BeamSearch(k, maxIterations);
+        State result;
+        if (useRR) result = searcher.BeamSearchWithRR(k, maxIterations);
+        else result = searcher.BeamSearch(k, maxIterations);
         long end = System.currentTimeMillis();
 
         if (result == null) System.out.println("Solution not found");
--- src/SpaceSearcher.java
+++ src/SpaceSearcher.java
@@ -25,6 +25,35 @@ public class SpaceSearcher {
                 childStates.addAll(currentState.getChildren());
             }
 
+            Collections.sort(childStates);
+            states.clear();
+
+            if (childStates.size() < k) {
+                states.addAll(childStates);
+            } else {
+                states.addAll(childStates.subList(0, k));
+            }
+            childStates.clear();
+        }
+
+        return null;
+    }
+
+    //Beam Search with soft restart (evades getting stuck in local minima)
+    State BeamSearchWithRR(int k, int maxIterations) { //k = beam nodes amount
+        for (int i = 0; i < k; i++) {
+            states.add(new State(dimension));
+        }
+
+        ArrayList<State> childStates = new ArrayList<>();
+        for (int i = 0; i < maxIterations; i++) {
+
+            while (states.size() > 0) {
+                State currentState = states.remove(0);
+                if (currentState.isFinal()) return currentState;
+                childStates.addAll(currentState.getChildren());
+            }
+
             //an exoume kolisei se local minima add random states in childStates
             if (childStates.size() == 0) {
                 for (int j = 0; j < k; j++) {
@@ -45,4 +74,5 @@ public class SpaceSearcher {
 
         return null;
     }
+
 }
