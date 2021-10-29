99cd82625f2da3498fca026b1895a157eb726065
Vasilis V <f3v3r.ath.cx@gmail.com>
2021-10-29 16:41:44 +0300

    fixed getting stuck in local minima

--- src/SpaceSearcher.java
+++ src/SpaceSearcher.java
@@ -6,15 +6,16 @@ public class SpaceSearcher {
     private int dimension;
     private ArrayList<State> states;
 
-    SpaceSearcher(int n){
+    SpaceSearcher(int n) {
         this.dimension = n;
         states = new ArrayList<>();
     }
 
-    State BeamSearch(int k, int maxIterations){ //k = beam nodes amount
+    State BeamSearch(int k, int maxIterations) { //k = beam nodes amount
         for (int i = 0; i < k; i++) {
             states.add(new State(dimension));
         }
+
         ArrayList<State> childStates = new ArrayList<>();
         for (int i = 0; i < maxIterations; i++) {
 
@@ -27,12 +28,18 @@ public class SpaceSearcher {
             Collections.sort(childStates);
             states.clear();
 
+            //an exoume kolisei se local minima add random states in childStates
+            if (childStates.size() == 0) {
+                for (int j = 0; j < k; j++) {
+                    childStates.add(new State(dimension));
+                }
+            }
+
             if (childStates.size() < k) {
                 states.addAll(childStates);
             } else {
                 states.addAll(childStates.subList(0, k));
             }
-
             childStates.clear();
         }
 
